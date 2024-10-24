9d1280cb78172245063ec229a31986567e1f8f52
Efren Garnica <efrengarnicapalacios@gmail.com>
2024-10-23 22:59:23 -0600

    Se sube mi primera app en React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import Testimonio from './componentes/Testimonio.js'
+import testimonios from './datos/testimonios.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen los programadores</h1>
+        {testimonios.map((testimonio, index) => (
+          <Testimonio
+            key={index}
+            nombre={testimonio.nombre}
+            pais={testimonio.pais}
+            imagen={testimonio.imagen}
+            cargo={testimonio.cargo}
+            empresa={testimonio.empresa}
+            testimonio={testimonio.testimonio}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/datos/testimonios.js
@@ -0,0 +1,28 @@
+const testimonios=[
+    {
+        nombre: 'Efren Garnica Palacios',
+        pais: 'México',
+        imagen: 'efren',
+        cargo: 'Actuario',
+        empresa: 'Genereishop',
+        testimonio: 'Innovación y Crecimiento Digital: Desde la creación de sitios web y aplicaciones móviles hasta la automatización de procesos en empresas y la inteligencia artificial, la programación se ha convertido en una herramienta clave para impulsar la innovación y el crecimiento en el mundo digital. Sin programación, muchas de las tecnologías que damos por sentadas hoy no existirían'
+      },
+      {
+        nombre: 'Iván Arratia',
+        pais: 'México',
+        imagen: 'ivan',
+        cargo: 'Ingeniero en Computación',
+        empresa: 'Genereishop',
+        testimonio: 'Desarrollo Personal y Profesional: Considera que el aprendizaje y la práctica de la programación no solo es una inversión en el desarrollo personal y profesional, sino también en el progreso colectivo de nuestra sociedad. La programación abre puertas a nuevas oportunidades laborales y permite a las personas contribuir de manera significativa a la evolución tecnológica'
+      },
+      {
+        nombre: 'Angel Andrade',
+        pais: 'México',
+        imagen: 'angel',
+        cargo: 'Desarrollador Java',
+        empresa: 'Genereishop',
+        testimonio: '<strong>Transformación</strong> de Industrias: Desde la salud hasta las finanzas, la manufactura y el entretenimiento, la programación impulsa la innovación y la eficiencia en diversas industrias. Este artículo explora cómo la programación está transformando sectores clave, destacando su importancia y el impacto que tiene en nuestra vida diaria'
+      }
+    ];
+    
+    export default testimonios;
